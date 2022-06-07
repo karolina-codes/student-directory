8c431c861e659bbb6d4f1074e5935613f470d682
Karolina Joksaite <k.joksaite00@gmail.com>
2022-06-07 12:33:32 +0100

    refactored student count

--- directory.rb
+++ directory.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: false
 
+student_count = 11
+
 puts 'The students of Villains Academy'
 puts '-------------'
 puts 'Dr. Hannibal Lecter'
@@ -13,6 +15,4 @@ puts 'Freddy Krueger'
 puts 'The Joker'
 puts 'Joffrey Baratheon'
 puts 'Norman Bates'
-print "Overall, we have "
-print 11
-puts " great students"
+print "Overall, we have #{student_count} great students"
