e4eb1d619e4b2dcaeb4f9e1322eb60ba276d2401
Karolina Joksaite <k.joksaite00@gmail.com>
2022-06-11 14:27:15 +0100

    fixed load_students bug

--- directory.rb
+++ directory.rb
@@ -39,6 +39,7 @@ def print_menu
   puts "1. Input the students"
   puts "2. Show the students"
   puts "3. Save the list to students.csv"
+  puts "4. Load the list from students.csv"
   puts "9. Exit"
 end
 
@@ -54,6 +55,8 @@ def process(selection)
     end
   when '3'
     save_students
+  when '4'
+    load_students
   when '9'
     exit
   else
@@ -74,11 +77,22 @@ def save_students
 
   @students.each do |student|
     student_data = [student[:name], student[:cohort]]
-    csv_line = student_data.join("\n")
+    csv_line = student_data.join(",")
     file.puts csv_line
   end
   file.close
 end
+
+def load_students 
+  file = File.open('students.csv', 'r')
+  
+  file.readlines.each do |line|
+    name, cohort = line.chomp.split(',')
+    @students << {name: name, cohort: cohort.to_sym}
+  end
+  file.close
+end
+
 
 interactive_menu
 
